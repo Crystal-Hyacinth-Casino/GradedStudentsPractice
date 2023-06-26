079cbe269b61cbf4ed11d31292606ebb749053a6
hiepnguyen90 <hiepnguyen@hieps-mbp.myfiosgateway.com>
2023-06-26 01:12:09 -0400

    finished student section

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,76 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores = new ArrayList<>();
+
+    public Student(String firstName, String lastName, Double[] testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        Collections.addAll(examScores, testScores);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getExamScores() {
+
+        String output = "Exam Scores:\n";
+
+        for (int i = 0; i < examScores.size(); i++) {
+        double score = examScores.get(i);
+        int roundedValue = (int) score ;
+
+        output += "       Exam " + (i + 1) + " -> " + roundedValue + "\n";
+
+        }
+        return output;
+    }
+
+    public int getNumberOfExamsTaken() {
+        return examScores.size();
+    }
+
+    public void addExamScore(Double examScore){
+        examScores.add(examScore);
+    }
+
+    public void setExamScore(int examNumber, Double newScore) {
+        examScores.set(examNumber, newScore);
+    }
+
+    public double getAverageExamScore(){
+        int sum = 0;
+
+        for(double d : examScores){
+            sum+= d;
+        }
+
+        return sum/getNumberOfExamsTaken();
+    }
+
+    @Override
+    public String toString() {
+        return "Student Name: " + getFirstName() + " " + getLastName() +
+                "\n> Average Score: " + (int)getAverageExamScore() +
+                "\n> " + getExamScores();
+
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,94 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
 public class StudentTest {
+
+    @Test
+    public void testGetExamScores(){
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { 100.0, 95.0, 123.0, 96.0 };
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        String output = student.getExamScores();
+
+        // Then
+        System.out.println(output);
+    }
+
+    @Test
+    public void testAddExamScore(){
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { };
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        student.addExamScore(100.0);
+        String output = student.getExamScores();
+
+        // Then
+        System.out.println(output);
+    }
+
+    @Test
+    public void testSetExamScore(){
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { 100.0 };
+        Student student = new Student(firstName, lastName, examScores);
+
+        String firstName2 = "Hiep";
+        String lastName2 = "Nguyen";
+        Double[] examScores2 = { 100.0 };
+        Student student2 = new Student(firstName2, lastName2, examScores2);
+
+        // When
+        student2.setExamScore(0, 160.0);
+        String output = student2.getExamScores();
+
+        student.setExamScore(0, 140.0);
+        String output2 = student.getExamScores();
+
+        // Then
+        System.out.println(output);
+        System.out.println(output2);
+    }
+
+    @Test
+    public void testGetAverageExamScore(){
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student = new Student(firstName, lastName, examScores);
+
+
+        // When
+        Double output = student.getAverageExamScore();
+
+        // Then
+        System.out.println(output);
+    }
+
+    @Test
+    public void testToString(){
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        String output = student.toString();
+
+        // Then
+        System.out.println(output);
+    }
 
 }
